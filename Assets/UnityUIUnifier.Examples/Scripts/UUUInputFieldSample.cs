6f4db3637e0ba5f0950865aa59e54fc95e53881f
tarukosu <tarukosu0@gmail.com>
2020-03-15 09:17:20 +0900

    Add UnifiedInputField

---
+++ Assets/UnityUIUnifier.Examples/Scripts/UUUInputFieldSample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UnityUIUnifier.Samples
+{
+    public class UUUInputFieldSample : MonoBehaviour
+    {
+        [SerializeField]
+        UnifiedInputField inputField = null;
+
+        private void Awake()
+        {
+            inputField.Text = "Default Text";
+
+            inputField.OnValueChanged += (text) =>
+            {
+                Debug.Log($"OnValueChanged: {text}");
+            };
+
+            inputField.OnEndEdit += (text) =>
+            {
+                Debug.Log($"OnEndEdit: {text}");
+            };
+        }
+    }
+}
